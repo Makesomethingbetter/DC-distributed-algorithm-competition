aed22e8706fb5f1ebfa230f07ffb2a22ea954bc3
Makesomethingbetter <724297316@qq.com>
2019-11-18 21:22:15 +0800

    Revert "1100加备注"
    This reverts commit ea78fa2b56e88f4cec5f8b8299380c1b4d7ef514.

--- javaSampleGood/src/main/java/Main.java
+++ javaSampleGood/src/main/java/Main.java
@@ -4,53 +4,33 @@ import conn.GeneralChannel;
 import json.Message;
 import json.config.Config;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static Config config;
 
-    public static void main(String[] args) throws Exception {
+//    public static double curTime() {
+//        return new Date().getTime() / 1000.0;
+//    }
 
+    public static void main(String[] args) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
-        config = objectMapper.readValue(new File("/home/config/client.json"), Config.class);
+        config = objectMapper.readValue(new File("client.json"), Config.class);
         Channel channel = new GeneralChannel();
         channel.initConfig(config);
-        //topo初始化
-        Topo topo=new Topo();
-        mainloop(channel,topo);
+        //
+        mainloop(channel);
     }
 
-
-    public static void mainloop(Channel channel,Topo topo){
-        Scheduler scheduler = new Scheduler(channel,topo);
-        int increaseInt=0;
-        int interval=3;
+    public static void mainloop(Channel channel){
+        Scheduler scheduler = new Scheduler(channel);
         while (true) {
             try {
                 List<Message> message = channel.recv();
                 for (Message msg : message) {
                     scheduler.onRecv(msg);
                 }
-                // while（true）每执行3次遍历TargetLimitNodesList
-                // 如果我可以建立通道则向TargetLimitNodesList里的点申请建立通道并将申请建立通道的点移除TargetLimitNodesList
-                // 如果不能连了，停止遍历TargetLimitNodesList
-                increaseInt++;
-                if (increaseInt%interval==0){
-                    if (topo.getTargetLimitNodesListSize()>0){
-                        ArrayList<Integer> list=(ArrayList<Integer>) topo.getTargetLimitNodesList();
-                        for (int i=0;i<list.size();i++){
-                            if (topo.canLinkAnyMore()){
-                                scheduler.sendChannelBuild(list.get(i),Const.STATE_REQUEST,Const.ERR_CODE_NONE,Const.CHANNEL_TYPE_FAST);
-                                topo.removeFromTargetLimitNodesList(list.get(i));
-                            }else{
-                                return;
-                            }
-                        }
-                    }
-                }
-                //message缓冲
                 Thread.sleep(20);
             } catch (InterruptedException e) {
                 break;
--- javaSampleGood/src/main/java/Scheduler.java
+++ javaSampleGood/src/main/java/Scheduler.java
@@ -8,18 +8,15 @@ public class Scheduler {
     private Topo topo;
 
 
-    public Scheduler(Channel channel,Topo topo) {
+    public Scheduler(Channel channel) {
         this.channel = channel;
         int N = Main.config.mainConfig.nodeCount;
-        this.topo=topo;
+        topo=new Topo();
         action = new Action[N+1];
-        // 其实不需要创建这么多对象，因为每一个action对象只有一个int target属性不同
-        // 不过无伤大雅毕竟毕竟服务器牛逼
         for (int i=1; i<=N; i++) {
             action[i] = new Action(i, this,topo);
         }
     }
-
 
     public int getId() {
         return channel.getId();
@@ -40,23 +37,24 @@ public class Scheduler {
                 if (message.channelId != 0) {
                     action[message.sysMessage.target].onSucc(message);
                 } else {
-                    if (message.state==Const.STATE_REFUSE){
-                        action[message.sysMessage.target].onRefuse(message.errCode,message.sysMessage.target);
+                    if (message.state==2){
+                        action[message.sysMessage.target].onRefuse(message.errCode);
                     }else if(message.state==Const.STATE_NOTICE){
                         action[message.sysMessage.target].onRequest(message);
                     }
                 }
                 break;
             case Const.CALL_TYPE_CHANNEL_DESTROY:
-                topo.deleteChannel(message.channelId);
-                    //通道destory说明我起码能再与对方创建一条通道
-                    //如果我有需要建立的连接，我就建立，因为这时我的连接数小于我的maxChannelConn
+                    topo.deleteChannel(message.channelId);
+                    topo.connCountToDoNumSubOne();
+                    //如果我还又需要建立的连接，我就建立，因为这时我的连接数小于我的maxChannelConn
                     //这时我可以连接通道，但是我不知道对方能不能连
-                    //如果对方不能连，server发来ERR_CODE_CHANNEL_BUILD_TARGET_LIMIT,将对方放入TargetLimitNodesList
-                    //TargetLimitNodesList将在main函数里进行轮询 见main第38行
+                    //如果对方不能连，对方发送拒绝信息（其实也可以不发），且对方记录下来我们之间需要channel
+                    //在对方能连的时候再向我发建立连接
                     if (!topo.nodeNeedToLinkButNotIsEmpty()){
                         int targetId=topo.getFirstNodeNeedToLinkButNot();
                         topo.removeFirstNodeNeedToLinkButNot();
+                        topo.connCountToDoNumAddOne();//新加的一行
                         sendChannelBuild(targetId, Const.STATE_REQUEST, Const.ERR_CODE_NONE, Const.CHANNEL_TYPE_FAST);
 
                     }
